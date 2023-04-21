ebe2ab3cf5e5a28d6bdaaee361a4d0d6272be5bd
Debaditya Bhar <debaditya.bhar@outlook.com>
2023-04-21 23:39:42 +0530

    Version 1.1.0 - Added Github Actions Build & Deploy script, Added Asset Prefix.

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,17 @@ import Footer from "./components/Footer";
 import Heading from "./components/Heading";
 import ReadMeApp from "./components/ReadMeApp";
 
+const assetPrefix = process.env.assetURL || "";
+
 export default function Home() {
   return (
     <>
       <Head>
         <title>ReadME Generator</title>
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-        <link rel="manifest" href="/favicon/site.webmanifest"></link>
+        <link rel="apple-touch-icon" sizes="180x180" href={`${assetPrefix}/favicon/apple-touch-icon.png`} />
+        <link rel="icon" type="image/png" sizes="32x32" href={`${assetPrefix}/favicon/favicon-32x32.png`} />
+        <link rel="icon" type="image/png" sizes="16x16" href={`${assetPrefix}/favicon/favicon-16x16.png`} />
+        <link rel="manifest" href={`${assetPrefix}/favicon/site.webmanifest`}></link>
       </Head>
       <Heading />
       <ReadMeApp />
