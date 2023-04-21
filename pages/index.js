1b5fe6dfcb6a78ffe6f0d8adf4ee7dd0d6388b11
Debaditya Bhar <debaditya.bhar@outlook.com>
2023-04-22 00:11:51 +0530

    Version 1.1.4 - Added meta description and changed the env variable name in build script

--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,24 @@ import Footer from "./components/Footer";
 import Heading from "./components/Heading";
 import ReadMeApp from "./components/ReadMeApp";
 
-const assetPrefix = process.env.assetURL || "";
+const assetPrefix = process.env.NEXT_PUBLIC_ASSET_URL || "";
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>ReadME Generator</title>
+
         <link rel="apple-touch-icon" sizes="180x180" href={`${assetPrefix}/favicon/apple-touch-icon.png`} />
         <link rel="icon" type="image/png" sizes="32x32" href={`${assetPrefix}/favicon/favicon-32x32.png`} />
         <link rel="icon" type="image/png" sizes="16x16" href={`${assetPrefix}/favicon/favicon-16x16.png`} />
-        <link rel="shortcut icon" href={`${assetPrefix}/favicon/favicon.ico`} />
         <link rel="manifest" href={`${assetPrefix}/favicon/site.webmanifest`}></link>
+
+         <meta
+          name="description"
+          content="This is Readme file generator app. This app display the readme file text written in markdown format, converts it to HTML format, copy the text into clipboad."
+        />
+
       </Head>
       <Heading />
       <ReadMeApp />
